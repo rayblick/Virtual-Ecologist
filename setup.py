02b106e196a0d1d77a6e3662c9f8e51a2fba1c1e
Ray <rblick.ecol@gmail.com>
2016-01-17 16:02:09 +1100

    created egg file and source file

--- setup.py
+++ setup.py
@@ -1,21 +1,20 @@
 from setuptools import setup, find_packages
 setup(
     name = "virtualecologist",
-    version = "0.1",
+    version = "0.0.1",
     #packages = find_packages(),
+    description = "Rationalise transect length in ecological surveys. Quantify"
+    "your error rate between surveys, or with a collegue, to determine"
+    "the optimal transect length in an ecological monitoring programme.",
 
     packages = ['virtualecologist','tests'],
-      long_description="""\
-      Rationalise transect length in ecological surveys. Quantify
-      your error rate between surveys, or with a collegue, to determine
-      the optimal transect length in an ecological monitoring programme.
-      """,
+      long_description="""""",
     #scripts = ['virtual_ecologist.py'],
 
     # Project uses reStructuredText, so ensure that the docutils get
     # installed or upgraded on the target machine
-    install_requires = ["csv", "pandas", "numpy", "math",\
-    "scipy", "matplotlib", "prettytable", "re"],
+    install_requires = ["csv", "pandas", "numpy",\
+    "scipy", "matplotlib", "prettytable"],
 
     package_data = {
         # If any package contains *.txt or *.rst files, include them:
@@ -23,12 +22,11 @@ setup(
         # And include any *.msg files found in the 'hello' package, too:
         '': ['*.msg'],
     },
-    data_files=[('example', ['example/fulldata.csv', 'example/pilotdata.csv'])]
+    data_files=[('example', ['example/fulldata.csv', 'example/pilotdata.csv'])],
 
-    # metadata for upload to PyPI
+    # metadata for upload to PyPi
     author = "Ray",
     author_email = "rblick.ecol@gmail.com",
-    description = "",
     license = "PSF",
     keywords = "ecology model virtual ecologist",
     url = "#",   # project home page, if any
